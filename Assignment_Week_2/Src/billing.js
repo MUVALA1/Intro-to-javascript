1d6a130cba54b4c6e88ea0d1b4615f71505a4f46
nyiramirembe <peninahhot@gmail.com>
2019-06-12 17:24:29 +0300

    new work

---
+++ Assignment_Week_2/Src/billing.js
@@ -0,0 +1,7 @@
+//This is the percentage that determines the transaction charge
+const transactionCharge=2.5%;
+var amountTendered=100000;
+let amount="This is what the billed amount is(less transaction charges)";
+console.log(amount);
+amount=amountTendered-transactionCharge
+console.log(amount);
