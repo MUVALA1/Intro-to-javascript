e0e365d58c765c5517fc71c75a4285758e028214
nyiramirembe <peninahhot@gmail.com>
2019-06-14 09:38:36 +0300

    change

--- null_checker.js
+++ null_checker.js
@@ -1,10 +1,11 @@
 for (var i = 1;i<=10;i++){
+	
 	let x=prompt('Enter your number',1);
-	if (x>0) {
+	
+	if (x>0) 
 		console.log(1);
 	else if(x<0)
 		console.log(-1);
-	else if(x==0)
+	else
 		console.log(0);		
-	}
 }
